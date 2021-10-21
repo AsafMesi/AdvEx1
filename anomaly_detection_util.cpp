6008ef767c95127e6f5be2c97981cb87a3ce1463
Dan Marom <danmarom16@outlook.com>
2021-10-21 14:58:44 +0300

    1St Commit by Dan Marom

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -26,3 +26,31 @@ float var(float* x, int size) {
     val = val/float(size);
     return val-u;
 }
+
+float cov(float* x, float* y, int size){
+    float sum = 0, mean1 = 0, mean2 = 0, a = 0, b = 0;
+    float testArr1[size], testArr2[size];
+
+    for (int i = 0; i < size; ++i) {
+        a += x[i];
+        b += y[i];
+    }
+    mean1 = a / (float) size;
+    mean2 = b / (float) size;
+
+    for (int i = 0; i < size; ++i) {
+
+        //test
+        testArr1[i] = x[i] - mean1;
+        testArr2[i] = y[i] - mean2;
+
+        sum += (x[i] - mean1) * (y[i] - mean2);
+    }
+    return sum / (float) size;
+}
+
+float pearson(float* x, float* y, int size) {
+    float covariance = cov(x, y, size);
+    float xSigma = sqrtf(var(x, size)), ySigma = sqrtf(var(y, size));
+    return (covariance / (xSigma * ySigma));
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "anomaly_detection_util.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    float  arr1[] = {10.5, 9.2, 8.1, 7.6, 6.2};
+    float arr2[] = {1.2, 2.1, 3.1, 4.6, 5.2};
+
+    // should be =~ -2.1108
+    std :: cout << cov(arr1, arr2, 5) << std :: endl;
+
+    // should be =~ 2.1176
+    std :: cout << var(arr1, 5) << std :: endl;
+
+    // should be =~ 2.2344
+    std :: cout << var(arr2, 5) << std :: endl;
+
+    // should be =~ -0.970386...
+    std :: cout << pearson(arr1, arr2, 5) << std :: endl;
 }
