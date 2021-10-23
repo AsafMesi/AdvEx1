a0e0e2f4f8552939418dd786333dedda670d069f
asafmesi <asafmcs8@gmail.com>
2021-10-23 01:16:13 -0700

    final touch

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -5,12 +5,8 @@
 #include <cmath>
 #include "anomaly_detection_util.h"
 
-
+// calculates the average of x array.
 float avg(float* x, int size) {
-    if(size == 0) {
-        std::cout << "empty array\n" << std::endl;
-        return 0;
-    }
     float sum = 0;
     for (int i = 0; i < size; ++i) {
         sum += x[i];
@@ -18,9 +14,10 @@ float avg(float* x, int size) {
     return sum/float(size);
 }
 
+//returns the variance of X and Y, output will be non-negative number by definition.
 float var(float* x, int size) {
     float val = 0, sum = 0, u;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; ++i) {
         val += powf(x[i], 2);
     }
     u = powf(avg(x, size), 2);
@@ -28,51 +25,49 @@ float var(float* x, int size) {
     return val-u;
 }
 
+// returns the covariance of X and Y
 float cov(float* x, float* y, int size){
-    float sum = 0, mean1 = 0, mean2 = 0, a = 0, b = 0;
-
+    float sum = 0, xAvg, yAvg;
+    xAvg = avg(x, size);
+    yAvg = avg(y, size);
     for (int i = 0; i < size; ++i) {
-        a += x[i];
-        b += y[i];
-    }
-    mean1 = a / (float) size;
-    mean2 = b / (float) size;
-
-    for (int i = 0; i < size; ++i) {
-        sum += (x[i] - mean1) * (y[i] - mean2);
+        sum += (x[i] - xAvg) * (y[i] - yAvg);
     }
     return sum / (float) size;
 }
 
+// returns the Pearson correlation coefficient of X and Y
 float pearson(float* x, float* y, int size) {
-    float covariance = cov(x, y, size);
-    float xSigma = sqrtf(var(x, size)), ySigma = sqrtf(var(y, size));
+    float covariance, xSigma, ySigma;
+    covariance = cov(x, y, size);
+    xSigma = sqrtf(var(x, size));
+    ySigma = sqrtf(var(y, size));
     return (covariance / (xSigma * ySigma));
 }
 
+// performs a linear regression and return s the line equation
 Line linear_reg(Point** points, int size) {
-    float xValue[size], yValue[size], a, b, xAvg = 0, yAvg = 0;
+    float xValue[size], yValue[size], a, b, xAvg, yAvg;
 
     // extract x and y values to different arrays, and sums up both averages.
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; ++i) {
         xValue[i] = points[i]->x;
         yValue[i] = points[i]->y;
     }
-
     xAvg = avg(xValue, size);
     yAvg = avg(yValue, size);
-
     a = (cov(xValue, yValue, size) / var(xValue, size));
     b = yAvg - (a * xAvg);
-
     return {a, b};
 }
 
+// returns the deviation between point p and the line equation of the points
 float dev(Point p,Point** points, int size){
     Line regLine = linear_reg(points, size);
     return dev(p, regLine);
 }
 
+// returns the deviation between point p and the line
 float dev(Point p,Line l){
     float fx = l.f(p.x);
     return fabs(fx-p.y);
