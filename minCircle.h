80c5487ebebbea6a39c0d553cad36411a4d76227
Dan Marom <danmarom16@outlook.co.il>
2021-12-11 15:35:45 +0200

    Version 3.0 - Ex3 Start

---
+++ minCircle.h
@@ -0,0 +1,38 @@
+//
+// Created by DanMa on 11/12/2021.
+//
+
+#ifndef ANOMALY_DETECTION_UTIL_CPP_MINCIRCLE_H
+#define ANOMALY_DETECTION_UTIL_CPP_MINCIRCLE_H
+
+
+#ifndef MINCIRCLE_H_
+#define MINCIRCLE_H_
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include "anomaly_detection_util.h"
+
+using namespace std;
+
+
+// ------------ DO NOT CHANGE -----------
+
+class Circle{
+public:
+    Point center;
+    float radius;
+    Circle(Point c,float r):center(c),radius(r){}
+};
+// --------------------------------------
+
+Circle findMinCircle(Point** points,size_t size);
+
+// you can add here additional methods
+
+#endif /* MINCIRCLE_H_ */
+
+
+#endif //ANOMALY_DETECTION_UTIL_CPP_MINCIRCLE_H
